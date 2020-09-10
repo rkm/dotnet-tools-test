c3639f73586214547bb82d6071cfd4bbb8dd7841
Ruairidh MacLeod <ruairidh@rkm.dev>
2020-09-10 08:50:20 +0000

    add proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_tools_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
