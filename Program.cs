ea349b5fd99cead80815d3e26c16afff881ff0dd
Ruairidh MacLeod <ruairidh@rkm.dev>
2020-09-10 10:36:21 +0000

    boop

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 
 namespace dotnet_tools_test
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
